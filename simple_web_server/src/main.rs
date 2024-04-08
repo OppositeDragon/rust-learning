7e2d860ea9d62ad089e2cdc60b8ad5bf666f271d
OppositeDragon <stee-ven_94@outlook.com>
2024-04-08 18:34:13 -0500

    Refactor handle_connection function in main.rs to improve HTTP response handling and 404 status

--- simple_web_server/src/main.rs
+++ simple_web_server/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
 };
@@ -15,17 +16,34 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    println!("Request: {:#?}", http_request);
-    // if let truncatedOne= http_request
-    let content=format!("<!DOCTYPE html><html lang=\"en\"><head> <meta charset=\"UTF-8\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <title>Document</title></head><body>{:?}</body></html>",http_request);
-    let content_length = content.len();
+
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "")
+    } else {
+        ("HTTP/1.1 404 Not Found", "404.html")
+    };
+    let response_contents = if filename == "" {
+        format!("<!DOCTYPE html><html lang=\"en\"><head> <meta charset=\"UTF-8\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Document</title></head><body><h1>Hello</h1><p>This is the response to <mark>{request_line}<mark/></p></body></html>")
+    } else {
+        fs::read_to_string(filename).unwrap().replace("AOEU", &request_line)
+    };
+    let response_length = response_contents.len();
     let response =
-        format!("HTTP/1.1 299 OK,but no rea\r\nContent-Length: {content_length}\r\n\r\n{content}",);
+        format!("{status_line}\r\nContent-Length: {response_length}\r\n\r\n{response_contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
+
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+    // println!("Request: {:#?}", http_request);
+    // let truncated_one = &http_request[1..http_request.iter().len()];
+    // let content=format!("<!DOCTYPE html><html lang=\"en\"><head> <meta charset=\"UTF-8\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <title>Document</title></head><body><h1>Hello</h1><p>{:#?}</p></body></html>",truncated_one);
+    // let content_length = content.len();
+    // let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {content_length}\r\n\r\n{content}",);
+
+    // stream.write_all(response.as_bytes()).unwrap();
 }
